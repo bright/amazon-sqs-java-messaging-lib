a23372ee1955c5f65d4dd0313b0cfb232ae40a54
maciej.nawrocki1 <maciej.nawrocki@practi.net>
2022-02-01 14:00:04 +0100

    Rename base package to com.amazon.sdkv2.sqs.javamessaging

---
+++ src/main/java/com/amazon/sdkv2/sqs/javamessaging/SQSConnectionFactory.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.amazon.sdkv2.sqs.javamessaging;
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.SqsClientBuilder;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+
+
+/**
+ * A ConnectionFactory object encapsulates a set of connection configuration
+ * parameters for <code>AmazonSQSClient</code> as well as setting
+ * <code>numberOfMessagesToPrefetch</code>.
+ * <P>
+ * The <code>numberOfMessagesToPrefetch</code> parameter is used to size of the
+ * prefetched messages, which can be tuned based on the application workload. It
+ * helps in returning messages from internal buffers(if there is any) instead of
+ * waiting for the SQS <code>receiveMessage</code> call to return.
+ * <P>
+ * If more physical connections than the default maximum value (that is 50 as of
+ * today) are needed on the connection pool,
+ * {@link com.amazonaws.ClientConfiguration} needs to be configured.
+ * <P>
+ * None of the <code>createConnection</code> methods set-up the physical
+ * connection to SQS, so validity of credentials are not checked with those
+ * methods.
+ */
+
+public class SQSConnectionFactory implements ConnectionFactory, QueueConnectionFactory {
+    private final ProviderConfiguration providerConfiguration;
+    private final AmazonSQSClientSupplier amazonSQSClientSupplier;
+    
+    /*
+     * At the time when the library will stop supporting Java 7, this can be removed and Supplier<T> from Java 8 can be used directly.
+     */
+    private interface AmazonSQSClientSupplier {
+        SqsClient get();
+    }
+
+    /*
+     * Creates a SQSConnectionFactory that uses AmazonSQSClientBuilder.standard() for creating AmazonSQS client connections.
+     * Every SQSConnection will have its own copy of AmazonSQS client.
+     */
+    public SQSConnectionFactory(ProviderConfiguration providerConfiguration) {
+        this(providerConfiguration, SqsClient.builder().build());
+    }
+    
+    /*
+     * Creates a SQSConnectionFactory that uses the provided AmazonSQS client connection.
+     * Every SQSConnection will use the same provided AmazonSQS client.
+     */
+    public SQSConnectionFactory(ProviderConfiguration providerConfiguration, final SqsClient client) {
+        if (providerConfiguration == null) {
+            throw new IllegalArgumentException("Provider configuration cannot be null");
+        }
+        if (client == null) {
+            throw new IllegalArgumentException("AmazonSQS client cannot be null");
+        }
+        this.providerConfiguration = providerConfiguration;
+        this.amazonSQSClientSupplier = new AmazonSQSClientSupplier() {
+            @Override
+            public SqsClient get() {
+                return client;
+            }
+        };
+    }
+    
+    /*
+     * Creates a SQSConnectionFactory that uses the provided AmazonSQSClientBuilder for creating AmazonSQS client connections.
+     * Every SQSConnection will have its own copy of AmazonSQS client created through the provided builder.
+     */
+    public SQSConnectionFactory(ProviderConfiguration providerConfiguration, final SqsClientBuilder clientBuilder) {
+        if (providerConfiguration == null) {
+            throw new IllegalArgumentException("Provider configuration cannot be null");
+        }
+        if (clientBuilder == null) {
+            throw new IllegalArgumentException("AmazonSQS client builder cannot be null");
+        }
+        this.providerConfiguration = providerConfiguration;
+        this.amazonSQSClientSupplier = new AmazonSQSClientSupplier() {
+            @Override
+            public SqsClient get() {
+                return clientBuilder.build();
+            }
+        };
+    }
+    
+    @Override
+    public SQSConnection createConnection() throws JMSException {
+        try {
+            SqsClient amazonSQS = amazonSQSClientSupplier.get();
+            return createConnection(amazonSQS, null);
+        } catch (RuntimeException e) {
+            throw (JMSException) new JMSException("Error creating SQS client: " + e.getMessage()).initCause(e);
+        }
+    }
+
+    @Override
+    public SQSConnection createConnection(String awsAccessKeyId, String awsSecretKey) throws JMSException {
+        AwsCredentialsProvider basicAWSCredentials = StaticCredentialsProvider.create(AwsBasicCredentials.create(awsAccessKeyId, awsSecretKey));
+        return createConnection(basicAWSCredentials);
+    }
+
+    public SQSConnection createConnection(AwsCredentials awsCredentials) throws JMSException {
+        AwsCredentialsProvider awsCredentialsProvider = StaticCredentialsProvider.create(awsCredentials);
+        return createConnection(awsCredentialsProvider);
+    }
+    
+    public SQSConnection createConnection(AwsCredentialsProvider awsCredentialsProvider) throws JMSException {
+        try {
+            SqsClient amazonSQS = amazonSQSClientSupplier.get();
+            return createConnection(amazonSQS, awsCredentialsProvider);
+        } catch(Exception e) {
+            throw (JMSException) new JMSException("Error creating SQS client: " + e.getMessage()).initCause(e);
+        }
+    }
+    
+    private SQSConnection createConnection(SqsClient amazonSQS, AwsCredentialsProvider awsCredentialsProvider) throws JMSException {
+        SqsMessagingClientWrapper amazonSQSClientJMSWrapper = new SqsMessagingClientWrapper(amazonSQS, awsCredentialsProvider);
+        return new SQSConnection(amazonSQSClientJMSWrapper, providerConfiguration.getNumberOfMessagesToPrefetch());
+    }
+    
+    @Override
+    public QueueConnection createQueueConnection() throws JMSException {
+        return (QueueConnection) createConnection();
+    }
+
+    @Override
+    public QueueConnection createQueueConnection(String userName, String password) throws JMSException {
+        return (QueueConnection) createConnection(userName, password);
+    }
+}
---
+++ src/test/java/com/amazon/sdkv2/sqs/javamessaging/UnorderedAcknowledgerTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.amazon.sdkv2.sqs.javamessaging;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import javax.jms.JMSException;
+
+import com.amazon.sdkv2.sqs.javamessaging.acknowledge.AcknowledgeMode;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import com.amazon.sdkv2.sqs.javamessaging.message.SQSMessage;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Test the UnorderedAcknowledgerTest class
+ */
+public class UnorderedAcknowledgerTest extends AcknowledgerCommon {
+
+    @Before
+    public void setupUnordered() throws JMSException {
+        amazonSQSClient = mock(SqsMessagingClientWrapper.class);
+        acknowledger = AcknowledgeMode.ACK_UNORDERED.createAcknowledger(amazonSQSClient, mock(SQSSession.class));
+    }
+
+    /**
+     * Test forgetUnAckMessages
+     */
+    @Test
+    public void testForgetUnAckMessages() throws JMSException {
+        int populateMessageSize = 30;
+        populateMessage(populateMessageSize);
+        
+        acknowledger.forgetUnAckMessages();
+        assertEquals(0, acknowledger.getUnAckMessages().size());
+    }
+
+    /**
+     * Test acknowledge does not impact messages that were not specifically acknowledge
+     */
+    @Test
+    public void testAcknowledge() throws JMSException {
+        int populateMessageSize = 37;
+        populateMessage(populateMessageSize);
+        int counter = 0;
+
+        List<SQSMessage> populatedMessagesCopy =  new ArrayList<SQSMessage>(populatedMessages);
+        while (!populatedMessagesCopy.isEmpty()) {
+
+            int rand = new Random().nextInt(populatedMessagesCopy.size());
+            SQSMessage message = populatedMessagesCopy.remove(rand);
+            message.acknowledge();
+            assertEquals(populateMessageSize - (++counter), acknowledger.getUnAckMessages().size());
+        }
+        assertEquals(0, acknowledger.getUnAckMessages().size());
+
+        ArgumentCaptor<DeleteMessageRequest> argumentCaptor = ArgumentCaptor.forClass(DeleteMessageRequest.class);
+        verify(amazonSQSClient, times(populateMessageSize)).deleteMessage(argumentCaptor.capture());
+
+        for (SQSMessage msg : populatedMessages) {
+            DeleteMessageRequest deleteRequest = DeleteMessageRequest.builder()
+                    .queueUrl(msg.getQueueUrl())
+                    .receiptHandle(msg.getReceiptHandle())
+                    .build();
+            assertTrue(argumentCaptor.getAllValues().contains(deleteRequest));
+        }
+    }
+}
